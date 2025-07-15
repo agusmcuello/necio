7f230e852748b29473aa84aeb549b36fc09ce65d
Agustín Martínez Cuello <105243844+agusmcuello@users.noreply.github.com>
2025-07-15 19:45:20 +0200

    fix deploy

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,13 +3,13 @@ import { cn } from "@/lib/utils";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   size?: "sm" | "lg";
 }
-
 export function Button({ size = "sm", className, ...props }: ButtonProps) {
   return (
     <button
       className={cn(
         "bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-xl cursor-pointer",
         size === "lg" ? "px-6 py-3 text-lg" : "px-4 py-2 text-base"
+        // ✅ acá lo estás usando correctamente
       )}
       {...props}
     />
